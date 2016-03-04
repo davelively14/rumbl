38c7070dbd15bd225313739195e172175e567d30
Dave Lively <davelively14@gmail.com>
2016-03-03 21:38:14 -0500

    create function for user_controller now uses registration_changeset

--- lib/rumbl/endpoint.ex
+++ lib/rumbl/endpoint.ex
@@ -11,6 +11,7 @@ defmodule Rumbl.Endpoint do
     at: "/", from: :rumbl, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
