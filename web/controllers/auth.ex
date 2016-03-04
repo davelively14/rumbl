90579de5eec9cb79eff6982960f14c0574aca28f
Dave Lively <davelively14@gmail.com>
2016-03-04 14:46:53 -0500

    Added login function to Rumbl.Auth

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -20,4 +20,15 @@ defmodule Rumbl.Auth do
     # Stores the user (or nil, if no existing session) in the :current_user
     assign(conn, :current_user, user)
   end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+
+    # Protects us from session fixation attacks. It tells Plug to send the session
+    # cookie back to the client with a different identifier, in case an attacker knew
+    # the previous one.
+    |> configure_session(renew: true)
+  end
 end
