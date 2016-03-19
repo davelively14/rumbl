883d667f97bafbe5a1f72bea6d5a3cf7257dd7a5
Dave Lively <davelively14@gmail.com>
2016-03-19 08:31:01 -0400

    Persists comments. Does not display them yet (next change)

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -2,20 +2,35 @@ defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
 
   def join("videos:" <> video_id, _params, socket) do
-    _ = video_id
-    {:ok, socket}
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
 
-  def handle_in("new_annotation", params, socket) do
+  # Ensures every incoming event has the current_user, then calls our
+  # handle_in/4 clause with the user as the 3rd argument.
+  def handle_in(event, params, socket) do
+    user = Repo.get(Rumbl.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
 
-    # Broadcast sends an event to all users on the current topic. It takes 3
-    # arguments: socket, name of the event, and a payload [an arbitrary map].
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at:   params["at"]
-    }
+  # Builds an annotation changeset for our user to persist the comment before
+  # broadcasting it on the channel.
+  def handle_in("new_annotation", params, user, socket) do
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
 
-    {:reply, :ok, socket}
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          id:   annotation.id,
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at:   annotation.at
+        }
+        {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
