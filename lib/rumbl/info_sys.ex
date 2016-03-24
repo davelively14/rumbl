8646461bb0b5cce1fed4013a6d10761019bb73bb
Dave Lively <davelively14@gmail.com>
2016-03-24 12:36:37 -0400

    Added concurrent timeout function

--- lib/rumbl/info_sys.ex
+++ lib/rumbl/info_sys.ex
@@ -36,9 +36,15 @@ defmodule Rumbl.InfoSys do
   end
 
   # Initial await_results
-  defp await_results(children, _opts) do
-    # Enters accumulation
-    await_result(children, [], :infinity)
+  defp await_results(children, opts) do
+    timeout = opts[:timeout] || 5000
+    # Once timeout expires, this will send :timedout to itself. :timedout is
+    # handled in our await_results/3 function below.
+    timer = Process.send_after(self(), :timedout, timeout)
+    results = await_result(children, [], :infinity)
+    # Calls cleanup function to clear out the timer once results are received
+    cleanup(timer)
+    results
   end
   # Recurses over spawned backends
   defp await_result([head|tail], acc, timeout) do
@@ -46,19 +52,40 @@ defmodule Rumbl.InfoSys do
 
     receive do
       {:results, ^query_ref, results} ->
+
         # Tries to receive next valid result for query, processes it, demonitors
         # it. Flush guarantees the :DOWN message will be removed from our inbox
         # in case it was delivered before we dropped the monitor.
         Process.demonitor(monitor_ref, [:flush])
         await_result(tail, results ++ acc, timeout)
+
       # Matches to monitor_ref (because that's where :DOWN messages occur), not
       # to query_ref.
       {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
         await_result(tail, acc, timeout)
+      :timedout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
     end
   end
   # Breaks recursion
   defp await_result([], acc, _) do
     acc
+  end
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    :erlang.cancel_timer(timer)
+
+    # Flush :timedout message from our inbox if it was sent
+    receive do
+      :timedout -> :ok
+    after
+      0 -> :ok
+    end
   end
 end
